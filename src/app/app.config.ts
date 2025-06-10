fd26c428b1a93762687ed16c0839d5230d0dac50
itzMiney <itzminey@proton.me>
2025-06-10 11:37:11 +0200

    Some updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,15 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
-import {provideHttpClient} from '@angular/common/http';
+import {HttpClient, provideHttpClient} from '@angular/common/http';
 import {AuthService} from './shared/services/auth.service';
 import {AuthInterceptor} from './auth.interceptor';
+import {provideTranslate, TranslateLoader} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +22,15 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideHttpClient(),
     AuthService,
-    AuthInterceptor
+    AuthInterceptor,
+
+    provideTranslate({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en',
+    }),
   ]
 };
