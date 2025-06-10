c52f9d6cc0106b63811ae7397d94df05a72798d4
itzMiney <itzminey@proton.me>
2025-06-10 12:06:03 +0200

    Dependency updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,15 +4,9 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideClientHydration, withEventReplay} from '@angular/platform-browser';
-import {HttpClient, provideHttpClient} from '@angular/common/http';
+import {provideHttpClient} from '@angular/common/http';
 import {AuthService} from './shared/services/auth.service';
 import {AuthInterceptor} from './auth.interceptor';
-import {provideTranslate, TranslateLoader} from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-export function HttpLoaderFactory(http: HttpClient): TranslateLoader {
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,14 +17,5 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(),
     AuthService,
     AuthInterceptor,
-
-    provideTranslate({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      },
-      defaultLanguage: 'en',
-    }),
   ]
 };
